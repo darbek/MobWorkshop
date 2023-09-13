fb7de925d968402440a2db4949495ac1f276a5fe
Dardan Bekteshi <dardan.bekteshi@maersk.com>
2023-09-13 11:06:13 +0200

    Initial commit

---
+++ src/test/java/org/example/StringCalculatorTest.java
@@ -0,0 +1,16 @@
+package org.example;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class StringCalculatorTest {
+    static class StringCalculator {
+    }
+
+    @Test
+    void name() {
+        var actual = 1 - 1;
+        assertEquals(0, actual);
+    }
+}
