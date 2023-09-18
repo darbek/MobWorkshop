40707d56bc9a5b0018c4d28a3d5e713131f6bb5c
Dardan Bekteshi <dardan.bekteshi@maersk.com>
2023-09-18 14:13:43 +0200

    Add assertion imports on PotterTest

--- src/test/java/org/example/PotterTest.java
+++ src/test/java/org/example/PotterTest.java
@@ -1,9 +1,11 @@
 package org.example;
 
 import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
 
 public class PotterTest {
     @Test
     void name() {
+        assertEquals(true, 0 == 0);
     }
 }
