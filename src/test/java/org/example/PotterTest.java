d0a15c77d5283093f0e3bdaf691912f719e1d2ec
Dardan Bekteshi <dardan.bekteshi@maersk.com>
2023-09-19 17:29:50 +0200

    String calculator and Potter implementations - group 1 session 1

--- src/test/java/org/example/PotterTest.java
+++ src/test/java/org/example/PotterTest.java
@@ -1,11 +1,68 @@
 package org.example;
 
 import org.junit.jupiter.api.Test;
+
+import java.util.Arrays;
+import java.util.List;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 public class PotterTest {
+
+    int book1;
+    int book2;
+    int book3;
+    int book4;
+    int book5;
+
+    public static int numBooks(List<Integer> books)
+    {
+        return (int) books.stream().filter(book -> book > 0).count();
+    }
+
+    public static double calculatePrice(List<Integer> books) {
+        int discountPercentage = 0;
+
+        switch (numBooks(books)){
+            case 3: {
+                discountPercentage = 10;
+            }
+            case 2: {
+                discountPercentage = 5;
+            }
+            case 1: {
+                discountPercentage = 0;
+            }
+        }
+
+        if (numBooks(books)>=2) {
+            discountPercentage = 5;
+        }
+
+        if (numBooks(books)>=3) {
+            discountPercentage = 5;
+        }
+
+        var pricePerBook = 8;
+        var totalPrice = (books.get(0)+books.get(1)+books.get(2)) * pricePerBook;
+        var discountedPrice = totalPrice * (100.00 - discountPercentage) / 100.00;
+        return discountedPrice;
+    }
+
     @Test
     void name() {
-        assertEquals(true, 0 == 0);
+        assertEquals(8, calculatePrice(Arrays.asList(1,0,0,0,0)));
+        assertEquals(16, calculatePrice(Arrays.asList(2,0,0,0,0)));
+        assertEquals(16 * 0.95, calculatePrice(Arrays.asList(1,1,0,0,0)));
+        assertEquals(16 * 0.95, calculatePrice(Arrays.asList(1,0,1,0,0)));
+        assertEquals(8* 3 * 0.90, calculatePrice(Arrays.asList(1,1,1,0,0)));
+    }
+
+    @Test
+    void numBooksTest() {
+        assertEquals(0, numBooks(Arrays.asList(0,0,0,0,0)));
+        assertEquals(2, numBooks(Arrays.asList(1,1,0,0,0)));
+        assertEquals(2, numBooks(Arrays.asList(2,1,0,0,0)));
+        assertEquals(5, numBooks(Arrays.asList(1,2,3,4,5)));
     }
 }
--- src/test/java/org/example/StringCalculatorTest.java
+++ src/test/java/org/example/StringCalculatorTest.java
@@ -2,15 +2,44 @@ package org.example;
 
 import org.junit.jupiter.api.Test;
 
+import java.util.stream.Stream;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class StringCalculatorTest {
     static class StringCalculator {
+        int add(String input){
+            if (input.isEmpty()) {
+                return 0;
+            }
+            String regex = "[,\n]";
+            if (input.startsWith("//")) {
+                regex = input.substring(2,3);
+                input = input.substring(4);
+            }
+            String[] numbers = input.split(regex);
+            var ints_stream = Stream.of(numbers).mapToInt(Integer::valueOf);
+            var negatives = ints_stream.filter( x-> x < 0);
+            return Stream.of(numbers).mapToInt(Integer::valueOf)
+                   /* .map(n-> {
+                        return n > 0 ? n : throw new RuntimeException("Negative numbers not allowed:" + n);
+                    })*/
+            .sum();
+        }
     }
 
     @Test
-    void name() {
-        var actual = 1 - 1;
-        assertEquals(0, actual);
+    void addMethodTest() {
+        StringCalculator sc = new StringCalculator();
+        assertEquals(0, sc.add(""));
+        assertEquals(1, sc.add("1"));
+        assertEquals(3, sc.add("1,2"));
+        assertEquals(10, sc.add("1,2,3,4"));
+        assertEquals(6, sc.add("1\n2,3"));
+        assertEquals(3,sc.add("//;\n1;2"));
+        assertEquals(3,sc.add("//a\n1a2"));
+        assertThrows(RuntimeException.class, () -> sc.add("1,4,-1"));
     }
+
+
 }
